c4be0738ae81bb714543dcda10b2e46f369658d4
LightStrawberry <L21828604@gmail.com>
2015-09-23 09:37:09 +0800

    add world change function

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+
+    private GameObject scene1;
+    private GameObject scene2;
+    private bool trigger;
+	// Use this for initialization
+	void Start () {
+        scene1 = GameObject.FindWithTag("Scene1");
+        scene2 = GameObject.FindWithTag("Scene2");
+        scene2.SetActive(false);
+        trigger = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            trigger = !trigger;
+            scene1.SetActive(trigger);
+            scene2.SetActive(!trigger);
+        }
+	}
+}
